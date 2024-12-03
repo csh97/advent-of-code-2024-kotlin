eea5bcc81dca5de943ce507e840ff27b0be3cfd8
Conor Haworth <conorhaworth@unknown.home>
2024-12-03 09:14:25 +0000

    Day 03

---
+++ src/day03.kt
@@ -0,0 +1,58 @@
+import kotlin.text.Regex
+
+val multiplyInstructionPattern = Regex("""mul\([0-9]{1,3},[0-9]{1,3}\)""")
+
+fun part1(input: String): Int {
+    val matches = multiplyInstructionPattern.findAll(input).map { it.value }.toList()
+
+    return matches.map {
+        it.extractMultiplyInstruction()
+    }.fold(0) { count, instruction ->
+        count + (instruction.first * instruction.second)
+    }
+}
+
+fun part2(input: String): Int {
+    val doPattern = Regex("""do\(\)""")
+    val dontPattern = Regex("""don't\(\)""")
+    val multiplyMaxLength = 12
+    val doMaxLength = 4
+    val dontMaxLength = 7
+
+    return input.map { it.toString() }.foldIndexed(0 to false) { i, (count, disabled), char ->
+        if (char == "m") {
+            val match = multiplyInstructionPattern.find(input.scanAhead(i, multiplyMaxLength))?.value
+            if (match != null && !disabled) {
+                val (a, b) = match.extractMultiplyInstruction()
+                return@foldIndexed count + (a * b) to false
+            }
+        }
+
+        if (char == "d") {
+            val doMatch = doPattern.find(input.scanAhead(i, doMaxLength))?.value
+            val dontMatch = dontPattern.find(input.scanAhead(i, dontMaxLength))?.value
+            if (doMatch != null) {
+                return@foldIndexed count to false
+            }
+            if (dontMatch != null) {
+                return@foldIndexed count to true
+            }
+        }
+
+        count to disabled
+    }.first
+}
+
+fun String.extractMultiplyInstruction(): Pair<Int, Int> {
+    val (a, b) = substringAfter("(").substringBefore(")").split(",")
+    return a.toInt() to b.toInt()
+}
+
+fun String.scanAhead(start: Int, end: Int): String = substring(start, (start+end).coerceAtMost(length))
+
+fun main() {
+    val input = "day03.txt".readFile()
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
